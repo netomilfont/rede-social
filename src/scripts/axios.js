6598fb19ab1d6d950bb720571442c7638156f2f1
netomilfont <netomilfont@hotmail.com>
2022-09-01 18:19:11 -0300

    feat: adiciona axios, toastify

---
+++ src/scripts/axios.js
@@ -0,0 +1,10 @@
+const token = localStorage.getItem("@kenzieSocial:token")
+
+export const instance = axios.create({
+    baseURL: "https://m2-rede-social.herokuapp.com/api/",
+    timeout: 1000,
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+    },
+})
