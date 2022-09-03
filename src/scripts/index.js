12381458e40eaf09945dd3b97cc9a84911895153
netomilfont <netomilfont@hotmail.com>
2022-09-03 15:39:14 -0300

    feat: abre modal e estilizacao modal

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,30 @@
+import { Modal } from "./modal.js"
 
+class Index {
+    // static renderIndex() {
+    //     const token = localStorage.getItem("@kenzieSocial:token")
+
+    //     if(token) {
+    //         window.location.replace("./src/pages/dashboard.html")
+    //     }
+    // }
+
+
+    static handleLoginModal() {
+        const loginBtn = document.querySelector("#loginBtn")
+
+        loginBtn.addEventListener("click", () => {
+            const newLoginModal = Modal.loginForm()
+            Modal.template(newLoginModal)
+            Index.closeModal()
+            Index.handleLogin()
+        })
+    }
+
+    // static closeModal() {
+    //     cosnt closeModalBtn = document.querySelector
+    // }
+
+}
+
+Index.handleLoginModal()
